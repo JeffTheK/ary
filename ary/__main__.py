893c92e5ae290a1bb7849fad6bdbbc2d7f63a959
JeffTheK <jeff144@protonmail.com>
2021-12-09 22:21:41 +0200

    Add basic main()

--- ary/__main__.py
+++ ary/__main__.py
@@ -1,2 +1,11 @@
+class Project:
+    def __init__(self, name, language, author="JeffTheK"):
+        self.name = name
+        self.language = language
+        self.author = author
+
 def main():
-    print("hello world")
+    name = input("name: ")
+    language = input("language: ")
+    project = Project(name, language)
+    print(f"creating {name} project written in {language}")
