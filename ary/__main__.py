25b74593232cac372ad242ab5ee28eeff79761da
JeffTheK <jeff144@protonmail.com>
2021-12-11 20:30:51 +0200

    Add basic project setupping

--- ary/__main__.py
+++ ary/__main__.py
@@ -5,3 +5,9 @@ def main():
     language = input("language: ")
     project = Project(name, language)
     print(f"creating {name} project written in {language}")
+
+    language = language.lower()
+    if language == "python":
+        setup_python_project()
+    else:
+        print(f"{language} not found")
